4048f38cf026824baefb2326fa13e073ad59ee07
janhvi chuahan <janhvic4100@gmail.com>
2026-04-28 13:27:20 +0200

    explore the data

--- Data_cleaning.R
+++ Data_cleaning.R
@@ -1,4 +1,13 @@
-install.packages("tidyverse")
+# Install relevant packages -------
+install.packages("pacman") # this needs to run only at the first instance. once it has run, comment this out
+
+pacman::p_load("tidyverse", "lubdridate", "janitor")
 
 
-library(tidyverse)
+
+
+blood <- read.csv("blood_donation.csv")
+ggplot(blood, aes(x=Blood_Group)) +
+  geom_bar(binwidth = 0.5)
+ggplot(blood, aes(x=Weight_kg, y=Hemoglobin_g_dL)) +
+  geom_point()
